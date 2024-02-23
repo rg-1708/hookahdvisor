10ee2fb5227c033f6cdf0d82ec991a782f5c6b32
Ramil Gojayev <gojayram@fit.cvut.cz>
2024-02-23 06:34:46 +0100

    Added login button

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,39 @@
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { LoginButton } from "@/components/auth/login-button";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
+
 export default function Home() {
-  return <main>empty</main>;
+  return (
+    <main
+      className="h-full 
+    flex flex-col items-center justify-center
+    bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800"
+    >
+      <div className="space-y-6 text-center">
+        <h1
+          className={cn(
+            "text-6xl font-semibold text-white drop-shadow-md",
+            font.className
+          )}
+        >
+          🔒 Auth
+        </h1>
+        <p className="text-white text-lg">Auth service</p>
+        <div>
+          <LoginButton>
+            <Button variant="secondary" size="lg">
+              Sign in
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
+  );
 }

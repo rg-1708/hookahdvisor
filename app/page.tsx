078baff0d89b907c5e31b623fb0048937d227e81
Ramil Gojayev <gojayram@fit.cvut.cz>
2024-02-23 07:00:00 +0100

    Added Custom Reusable Card Wrapper for auth components.

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
     <main
       className="h-full 
     flex flex-col items-center justify-center
-    bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800"
+   bg-primary-gradient"
     >
       <div className="space-y-6 text-center">
         <h1

7e4c1cd1eceac00374138c8dc679313333e44471
Ramil Gojayev <gojayram@fit.cvut.cz>
2024-02-23 08:41:15 +0100

    Added register page, form and server action

--- schemas/index.ts
+++ schemas/index.ts
@@ -6,3 +6,11 @@ export const LoginSchema = z.object({
     message: "Password is required",
   }),
 });
+
+export const RegisterSchema = z.object({
+  email: z.string().email({ message: "Email is required" }),
+  password: z.string().min(6, {
+    message: "Minimum 6 charachters are required",
+  }),
+  name: z.string().min(1, { message: "Name is required" }),
+});
